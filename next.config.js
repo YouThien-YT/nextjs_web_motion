460488ba387a37b8c02d5eed12240886561fea65
You Thiên <76463266+YouThien-YT@users.noreply.github.com>
2023-07-24 23:06:36 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+  // reactStrictMode: true,
+  // swcMinify: true,
+  basePath: '/nextjs_web_motion',
+  assetPrefix: '/nextjs_web_motion/',
 }
 
 module.exports = nextConfig
