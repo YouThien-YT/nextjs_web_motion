db51076f87bd3f68104432452ea1a6f50332c998
You Thiên <76463266+YouThien-YT@users.noreply.github.com>
2023-08-09 09:09:06 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ const Home = () => {
       {/* image */}
       <div className="w-[1200px] h-full absolute right-0 bottom-0">
         {/* xl:bg_img */}
-        <div className={`bg-none xl:bg-[url(https://lh3.googleusercontent.com/pw/AIL4fc_flXbgEjkd1tqfHvPNvurbyJBH_ZKqF46NrdHjhsRJCHng5bmKIXui7Dkq90O4Xgb4_DSMxF1NvHhbg7c2JOfGErwMvLduR2HVXLNP5CN0CgPlBZFW9pz0xZo3jTi5wem5RvTRGQLWD5nRpI1tN7aT5oenEHpVSX6Ps9M7D1wyJXGmR5QGgqslNm_iwhwS_fJ-_n4y8Ubu-T02Vm7rIKf--EfxPHH1nfJGSz6QNYEX8D_HMkS4vGe1sUp_kSkETgKUijIWux9zURaC5D1D3YRmH0trte1z6altqOdWBtt7XBETDoUQ8bjxA1U04_YEQxOpZtxYyQOaR--7A0awUZd05vHrVQENDMywir-akWKoodYq4IZUrCaVfwXYfzjzw9fkN8uSDGVZD_bTxIA9hUyjU67QlBj9UTj-jLs3P79j13kiFSbTS2QgYuM7oWdz30v0KwNqpm0GayCUqc4Kr_IYy9uuRaV9B2QAofo8mjBQ9TZ30odxhwXtIYw64lrPXof9CTYvPxzzfZa3IRHSTlzdYGD3Ro3HhaTf_7vWj_cYjRwvAIAeHhZzQ7_Si_Svxk64_ti4zqWGuwmh0Zt8XlKSMW-90O6yVD0ocl5hA8KDgbpFhhv6L3jQN9YHEJscgKOnrp289m2D19M3F6cW-1wHM9GCFebEfj3f1jzRYi2yawYCuRSvhNNt6PmyLdsoB8sQEYBlS7j32n17NHbzbwja8MtTF2icLQheC6ZH_kB6rJwR0szIdSToyOdVcc0t5FI1QBENZjWNqmjjl11XTUKlE4XvOhA3z_cy2b55lohz7eI02bnIXgL3K_Lsz8Vsh6ShHcs0wU1Rzyccmto8SKQTaXkhQX7DFgQi3S0nsFZIEnL5ZD1Z5FyiiySiLpciyf3ZHJWWPJ1aAHq7wxK-fKweAwo=w1052-h619-s-no?authuser=0)] xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0 `}>
+        <div className={`bg-none xl:bg-[url(https://i.imgur.com/miiIfUM.png)] xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0 `}>
         </div>
         {/* particles */}
         <ParticlesContainer></ParticlesContainer>
